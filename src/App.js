6a8ae64ccaac39159ff5c57821d3f5abf591d635
John <jsavage1498@gmail.com>
2020-11-21 19:07:22 -0800

    added feature

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Donkfffey
+          This is a change from the feature branch
         </p>
         <a
           className="App-link"
